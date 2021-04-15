bc98adbc724e3b221884688baff10d8158b34238
Gurtubay <44202873+Gurtubay@users.noreply.github.com>
2021-04-15 14:41:45 -0500

    Update RetoPOO.cpp
    Tarea 2, completa con 2 casos prueba

--- RetoPOO.cpp
+++ RetoPOO.cpp
@@ -10,6 +10,12 @@ class Media{
         int calificacion;
 
     public:
+        Media(string nom, float dur, string gen, int calif){
+            nombre = nom;
+            duracion = dur;
+            genero = gen;
+            calificacion = calif;
+        }
         string getNombre(){
             return nombre;
         }
@@ -33,8 +39,13 @@ class Media{
 }; //Clase Media
 
 class Pelicula : public Media{
-
+    public:
+        Pelicula(string, float, string, int);
 };//Clase hija Pelicula
+
+Pelicula::Pelicula(string _nombre, float _duracion, string _genero, int _calificacion):Media(_nombre,_duracion,_genero,_calificacion){
+
+}
 
 class  Serie : public Media{
     public:
@@ -42,15 +53,33 @@ class  Serie : public Media{
         int temporada;
     
     public:
+        Serie(string, float, string, int, int, int);
         int getEpisodio(){
             return episodio;
         }
         int  getTemporada(){
             return temporada;
         }
+        void print(){
+            cout << "Nombre: "  << nombre << endl;
+            cout << "Duracion: "  << duracion << endl;
+            cout << "Genero: "  << genero << endl;
+            cout << "Calificacion: "  << calificacion << endl;
+            cout << "Episodio: "  << episodio << endl;
+            cout << "Temporada: "  << temporada << endl;
+            
+        }
 }; //Clase hija Serie
 
+Serie::Serie(string _nombre, float _duracion, string _genero, int _calificacion, int _episodio, int _temporada):Media(_nombre,_duracion,_genero,_calificacion){
+    episodio = _episodio;
+    temporada = _temporada;
+}
+
 int main (){
-    Serie epi;
-        epi.nombre="Invincible";
-        epi.
+    Pelicula avenger("Avengers", 2.5, "accion", 5);
+    avenger.imprimir();
+    Serie vikings("Vikings", 1.0, "Historia", 4, 5, 6);
+    vikings.print();
+    return 0;
+}
